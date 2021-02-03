a5bb5a949ec7c12408dd643d903b934a726cd158
santoshi <kalaskars1996@gmail.com>
2021-02-03 17:49:09 +0530

    Face movement detection..!

--- Face_movement_detection.py
+++ Face_movement_detection.py
@@ -16,11 +16,13 @@ class FaceMovementDetection:
             self.faces = self.detector(self.gray_frame)
             self.local_face_count = 0
             self.local_no_face_count = 0
+
+            # No Face in Frame
             if not self.faces:
                 self.local_no_face_count = self.local_no_face_count + 1
                 cv2.putText(self.frame, "No Face Detected..! ", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 210, 90), 2)
             for face in self.faces:
-
+                # count No of Faces
                 self.local_face_count = self.local_face_count + 1
                 if self.local_face_count > 1:
                     cv2.putText(self.frame, "Face Count: " + str(self.local_face_count), (10, 150),
@@ -38,11 +40,12 @@ class FaceMovementDetection:
                 Right_side_diff = int(np.sqrt((Center_x_point_31 - Right_x_point_15) ** 2 + (Center_y_point_31 - Right_y_point_15) ** 2))
                 Left_side_percentage = int((Left_side_diff / Main_diff_3_to_15) * 100)
                 Right_side_percentage = int((Right_side_diff / Main_diff_3_to_15) * 100)
-                for landmark_points in range(0, 68):
-                    cv2.line(self.frame, (Left_x_point_3, Left_y_point_3), (Center_x_point_31, Center_y_point_31), (0, 255, 0), 3)
-                    cv2.line(self.frame, (Right_x_point_15, Right_y_point_15), (Center_x_point_31, Center_y_point_31), (0, 255, 0), 3)
-                    cv2.putText(self.frame, "Looking at Right " + str(Left_side_percentage), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 210, 0), 2)
-                    cv2.putText(self.frame, "Looking at Left " + str(Right_side_percentage), (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 100, 10), 2)
+
+                # Drawing Lines
+                cv2.line(self.frame, (Left_x_point_3, Left_y_point_3), (Center_x_point_31, Center_y_point_31), (0, 255, 0), 3)
+                cv2.line(self.frame, (Right_x_point_15, Right_y_point_15), (Center_x_point_31, Center_y_point_31), (0, 255, 0), 3)
+                cv2.putText(self.frame, "Right side Distance" + str(Left_side_percentage), (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 210, 0), 2)
+                cv2.putText(self.frame, "Left Side Distance" + str(Right_side_percentage), (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 100, 10), 2)
 
             cv2.imshow("Web_cam_on", self.frame)
             escap_key = cv2.waitKey(1)
