17979a639d76e25aac9d46f728e6d29be3671120
santoshi <kalaskars1996@gmail.com>
2021-02-08 18:48:56 +0530

    Refactored : training faces encodings from image path..!

--- Face_movement_detection.py
+++ Face_movement_detection.py
@@ -10,9 +10,6 @@ class FaceMovementDetection:
         self.video_cap = cv2.VideoCapture(0)
         self.detector = dlib.get_frontal_face_detector()
         self.predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-        self.name = "santoshi kalaskar"
-        self.training_image_path = os.path.join("Training_images", self.name)
-        self.training_dataset_path = os.path.join("Training_dataset", self.name)
         self.local_no_face_count = 0
         self.global_no_face_counter = 0
         self.local_more_than_one_face_counter = 0
@@ -21,17 +18,24 @@ class FaceMovementDetection:
         self.global_left_right_counter = 0
         self.fps = self.video_cap.get(cv2.CAP_PROP_FPS)
         print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(self.fps))
-        self.training_images, self.training_img_names = self.Create_Image_Name_list()
+        # self.training_images, self.training_img_names = self.Create_Image_Name_list()
 
-    def Create_Image_Name_list(self):
+    def GetTrainingData(self):
+        self.name = "santoshi kalaskar"
+        self.training_image_path = os.path.join("Training_images", self.name)
+        # self.training_dataset_path = os.path.join("Training_dataset", self.name)
         self.training_images = []
         self.training_img_names = []
+        self.training_face_encoding = []
         self.myImageList = os.listdir(self.training_image_path)
         for img_list in self.myImageList:
             current_img = cv2.imread(f'{self.training_image_path}/{img_list}')
             self.training_images.append(current_img)
             self.training_img_names.append(os.path.splitext(img_list)[0])
-        return self.training_images, self.training_img_names
+            current_img_path = os.path.join(self.training_image_path, img_list)
+            self.Current_image = face_recognition.load_image_file(current_img_path)
+            self.training_face_encoding.append(face_recognition.face_encodings(self.Current_image)[0])
+        # return self.training_images, self.training_img_names, self.training_face_encoding
 
     def StopExam(self,warning_msg):
         print(warning_msg)
@@ -40,50 +44,29 @@ class FaceMovementDetection:
 
     def StartWebCam(self):
 
-        obama_image = face_recognition.load_image_file("Training_images/santoshi kalaskar/santoshi kalaskar.jpg")
-        obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-        # Create arrays of known face encodings and their names
-        known_face_encodings = [
-            obama_face_encoding,
-        ]
-        known_face_names = [
-            "Santoshi Kalaskar",
-        ]
-        # Initialize some variables
-        face_locations = []
-        face_names = []
-        process_this_frame = True
-
-
         while True:
             _, self.frame = self.video_cap.read()
 
             # Creating object of Brightness class
-            self.frame = cv2.convertScaleAbs(self.frame, alpha=1.5, beta=1)
+            # self.frame = cv2.convertScaleAbs(self.frame, alpha=1.5, beta=1)
             ''' ---------------------------------------------------------------------'''
             small_frame = cv2.resize(self.frame, (0, 0), fx=0.25, fy=0.25)
             # Convert the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
             rgb_small_frame = small_frame[:, :, ::-1]
-            # Only process every other frame of video to save time
-            if process_this_frame:
-                # Find all the faces and face encodings in the current frame of video
-                face_locations = face_recognition.face_locations(rgb_small_frame)
-                face_encodings1 = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
 
-                face_names = []
-                for face_encoding in face_encodings1:
-                    # See if the face is a match for the known face(s)
-                    matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-                    name = "Unknown"
-                    # Or instead, use the known face with the smallest distance to the new face
-                    face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
-                    best_match_index = np.argmin(face_distances)
-                    if matches[best_match_index]:
-                        name = known_face_names[best_match_index]
-
-                    face_names.append(name)
-
-            process_this_frame = not process_this_frame
+            face_names = []
+            for face_encoding in face_encodings:
+                name = "Unknown"
+                # See if the face is a match for the known face(s)
+                matches = face_recognition.compare_faces(self.training_face_encoding, face_encoding)
+                # Or instead, use the known face with the smallest distance to the new face
+                face_distances = face_recognition.face_distance(self.training_face_encoding, face_encoding)
+                best_match_index = np.argmin(face_distances)
+                if matches[best_match_index]:
+                    name = self.training_img_names[best_match_index]
+                face_names.append(name)
 
             # Display the results
             for (top, right, bottom, left), name in zip(face_locations, face_names):
@@ -96,8 +79,7 @@ class FaceMovementDetection:
                 cv2.rectangle(self.frame, (left, top), (right, bottom), (0, 0, 255), 2)
                 # Draw a label with a name below the face
                 cv2.rectangle(self.frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
-                font = cv2.FONT_HERSHEY_DUPLEX
-                cv2.putText(self.frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+                cv2.putText(self.frame, name, (left + 6, bottom - 6), cv2.FONT_HERSHEY_DUPLEX, 1.0, (255, 255, 255), 1)
 
             ''' ---------------------------------------------------------------------'''
             self.gray_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
@@ -168,4 +150,5 @@ class FaceMovementDetection:
 
 if __name__ == "__main__":
     FaceMovementDetection_obj = FaceMovementDetection()
+    FaceMovementDetection_obj.GetTrainingData()
     FaceMovementDetection_obj.StartWebCam()
